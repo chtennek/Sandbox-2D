7c1184a4e82c938220bc2836eb453c52be949d90
GoodKenneth <kenthesage@gmail.com>
2018-01-01 05:15:26 -0500

    Add barebones platformer movement

---
+++ Assets/Scripts/Controls/SidescrollerJump.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidescrollerJump : MonoBehaviour
+{
+    // [TODO] implement multijump
+    public float jumpSpeed = 8f;
+    public float defaultGravityScale = 2f;
+    public float jumpReleaseGravityScale = 12f;
+    public float fallGravityScale = 4f;
+    public int maxDoubleJumps = 1;
+
+    private List<Collider2D> groundTouched = new List<Collider2D>();
+    private int doubleJumpsLeft = 1;
+
+    private InputReceiver input;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        input = GetComponent<InputReceiver>();
+        rb = GetComponent<Rigidbody2D>();
+        doubleJumpsLeft = maxDoubleJumps;
+    }
+
+    private void FixedUpdate()
+    {
+        // Refresh double jumps
+        if (groundTouched.Count > 0)
+        {
+            doubleJumpsLeft = maxDoubleJumps;
+        }
+
+        // Jump if we are able to
+        if (input.Jump() && (doubleJumpsLeft > 0 || groundTouched.Count > 0))
+        {
+            float targetSpeed = jumpSpeed - rb.velocity.y;
+            rb.AddForce(targetSpeed * Vector2.up, ForceMode2D.Impulse);
+            if (groundTouched.Count == 0)
+            {
+                doubleJumpsLeft--;
+            }
+        }
+
+        // Tweak jump trajectory
+        rb.gravityScale = defaultGravityScale;
+        if (input.JumpRelease())
+        {
+            rb.gravityScale = jumpReleaseGravityScale;
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallGravityScale;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ContactPoint2D[] points = new ContactPoint2D[10];
+        collision.GetContacts(points);
+        foreach (ContactPoint2D p in points)
+        {
+            if (p.normal == Vector2.up && !groundTouched.Contains(collision.collider))
+            {
+                groundTouched.Add(collision.collider);
+                return;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        groundTouched.Remove(collision.collider);
+    }
+}
--- Assets/Scripts/Input/InputReceiver.cs
+++ Assets/Scripts/Input/InputReceiver.cs
@@ -5,23 +5,77 @@ using Rewired;
 
 public class InputReceiver : MonoBehaviour
 {
-	public int playerId = 0;
-	public Player player;
-	
-	void Awake()
-	{
-		player = ReInput.players.GetPlayer(playerId);
-	}
+    public int playerId = 0;
+    public Player player;
 
-	public Vector3 GetNormalizedMovementVector()
-	{
-		return GetMovementVector().normalized;
-	}
+    private bool jumpBuffered;
+    private bool jumpReleaseBuffered;
+    private Vector2 currentMovementVector;
 
-	public Vector3 GetMovementVector()
-	{
-		float horizontal = player.GetAxisRaw("Move Horizontal");
-		float vertical = player.GetAxisRaw("Move Vertical");
-		return new Vector3(horizontal, vertical);
-	}
+    private void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+    }
+
+    private void Update()
+    {
+        if (player.GetButtonDown("Jump"))
+        {
+            jumpBuffered = true;
+        }
+        if (player.GetButtonUp("Jump"))
+        {
+            jumpReleaseBuffered = true;
+        }
+        float x = player.GetAxisRaw("Move Horizontal");
+        float y = player.GetAxisRaw("Move Vertical");
+        currentMovementVector = new Vector2(x, y);
+    }
+
+    public Vector3 GetQuantizedMovementVector()
+    {
+        Vector2 output = currentMovementVector;
+        if (output.x > 0)
+        {
+            output.x = 1;
+        }
+        else if (output.x < 0)
+        {
+            output.x = -1;
+        }
+        if (output.y > 0)
+        {
+            output.y = 1;
+        }
+        else if (output.y < 0)
+        {
+            output.y = -1;
+        }
+        return output;
+    }
+
+    public Vector3 GetMovementVector()
+    {
+        return currentMovementVector;
+    }
+
+    public bool JumpRelease()
+    {
+        if (jumpReleaseBuffered)
+        {
+            jumpReleaseBuffered = false;
+            return true;
+        }
+        return false;
+    }
+
+    public bool Jump()
+    {
+        if (jumpBuffered)
+        {
+            jumpBuffered = false;
+            return true;
+        }
+        return false;
+    }
 }
