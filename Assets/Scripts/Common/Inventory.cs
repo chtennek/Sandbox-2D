88e464c1ff877e657b567fa2415c2283d9fce7c8
GoodKenneth <kenthesage@gmail.com>
2018-01-20 02:20:29 -0500

    Implement BorderedTile

---
+++ Assets/Scripts/Common/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory Item")]
+public class Inventory : MonoBehaviour
+{
+    public List<InventoryItem> items = new List<InventoryItem>();
+    public int maxSpace;
+
+    private void Awake()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Common/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : ScriptableObject
+{
+    public string itemName;
+    public int currentStack = 1;
+    public int maxPerStack = 1;
+
+    public Sprite image;
+
+    public void OnUse()
+    {
+
+    }
+}
