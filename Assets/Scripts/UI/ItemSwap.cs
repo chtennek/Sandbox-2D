fab7fa3631576639cc8126909ad031399d0db9d0
GoodKenneth <kenthesage@gmail.com>
2018-03-01 11:15:18 -0500

    Add ItemSwap

---
+++ Assets/Scripts/UI/ItemSwap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSwap : MonoBehaviour, ISubmitHandler
+{
+    public static ItemSwap selected;
+
+    public void OnSubmit(BaseEventData eventData)
+    {
+        if (selected == this) {
+            selected = null;
+            return;
+        }
+
+        if (selected != null && transform.parent != null && selected.transform.parent == transform.parent) {
+            int i = transform.GetSiblingIndex();
+            int j = selected.transform.GetSiblingIndex();
+            transform.SetSiblingIndex(j);
+            selected.transform.SetSiblingIndex(i);
+
+            EventSystem.current.SetSelectedGameObject(selected.gameObject);
+            selected = null;
+            return;
+        }
+
+        selected = this;
+    }
+}
