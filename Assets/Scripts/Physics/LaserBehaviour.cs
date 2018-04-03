677f6355be76a20d3dcc0002a829a895b3b4e35d
GoodKenneth <kenthesage@gmail.com>
2018-04-03 00:58:40 -0400

    Add assets for lasers and particles

---
+++ Assets/Scripts/Physics/LaserBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBehaviour : MonoBehaviour
+{
+    private LineRenderer[] lines;
+
+    private void Awake()
+    {
+        lines = GetComponentsInChildren<LineRenderer>();
+    }
+}
