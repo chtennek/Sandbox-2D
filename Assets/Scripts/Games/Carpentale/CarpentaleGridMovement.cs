a08c637147e52980fb6c60ca061cff52d77dd3f9
GoodKenneth <kenthesage@gmail.com>
2018-04-09 01:57:37 -0400

    Add merge particle effect

--- Assets/Scripts/Games/Carpentale/CarpentaleGridMovement.cs
+++ Assets/Scripts/Games/Carpentale/CarpentaleGridMovement.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarpentaleGridMovement : GridMovement {
+public class CarpentaleGridMovement : GridMovement
+{
     [Header("Merging")]
+    public Transform mergeEffectPrefab;
     public string mergeObjectsWithTag = "Block";
     public float mergeTime = 1f;
     public float mergeContributionWindow = 1f;
@@ -11,7 +13,7 @@ public class CarpentaleGridMovement : GridMovement {
     private float mergeStartTime;
     private HashSet<Transform> mergeablesLastFrame;
 
-	public override bool Move(Vector3 v, bool fixRotation)
+    public override bool Move(Vector3 v, bool fixRotation)
     {
         if (IsMoving == true)
             return false;
@@ -24,7 +26,8 @@ public class CarpentaleGridMovement : GridMovement {
         HashSet<GridMovement> allAffected = GetAffectedObjectsAlong(movement, true);
 
         // Check if there's something in the way
-        if (pushables.SetEquals(allAffected)) {
+        if (pushables.SetEquals(allAffected))
+        {
             mergeablesLastFrame = null;
             foreach (GridMovement g in pushables)
                 g.Push(movement, fixRotation);
@@ -32,17 +35,21 @@ public class CarpentaleGridMovement : GridMovement {
         }
 
         HashSet<Transform> mergeables = FindMergeables(allAffected);
-        if (mergeablesLastFrame == null || mergeables.SetEquals(mergeablesLastFrame) == false) {
+        if (mergeablesLastFrame == null || mergeables.SetEquals(mergeablesLastFrame) == false)
+        {
             mergeablesLastFrame = mergeables;
             mergeStartTime = Time.time;
         }
-        else if (Time.time - mergeStartTime >= mergeTime) {
+        else if (Time.time - mergeStartTime >= mergeTime)
+        {
+            SpawnEffectsAtContactPoints(mergeables);
             MergeObjects(mergeables);
         }
         return false;
-	}
+    }
 
-    private HashSet<Transform> FindMergeables(HashSet<GridMovement> objects) {
+    private HashSet<Transform> FindMergeables(HashSet<GridMovement> objects)
+    {
         HashSet<Transform> mergeables = new HashSet<Transform>();
         foreach (GridMovement obj in objects)
         {
@@ -53,34 +60,46 @@ public class CarpentaleGridMovement : GridMovement {
     }
 
     // [TODO] performance
-    private List<Vector3> FindContactPoints(HashSet<Transform> mergeables) {
-        List<Vector3> contactPoints = new List<Vector3>();
+    private void SpawnEffectsAtContactPoints(HashSet<Transform> mergeables)
+    {
+        if (mergeEffectPrefab == null)
+            return;
+        
         List<Vector3> cubePoints = new List<Vector3>();
-        foreach (Transform container in mergeables) {
-            foreach (Transform child in container) {
-                foreach (Vector3 point in cubePoints) {
-                    if (Vector3.Distance(point, child.position) == 1) {
-                        contactPoints.Add(.5f * (point + child.position));
+        foreach (Transform container in mergeables)
+        {
+            foreach (Transform child in container)
+            {
+                foreach (Vector3 point in cubePoints)
+                {
+                    if (Vector3.Distance(point, child.position) == 1)
+                    {
+                        Vector3 position = .5f * (point + child.position);
+                        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, point - child.position);
+                        Instantiate(mergeEffectPrefab, position, rotation);
                     }
                 }
             }
-            foreach (Transform child in container) {
+            foreach (Transform child in container)
+            {
                 cubePoints.Add(child.position);
             }
         }
-        return contactPoints;
     }
 
-    private void MergeObjects(HashSet<Transform> mergeables) {
+    private void MergeObjects(HashSet<Transform> mergeables)
+    {
 
         List<Transform> children = new List<Transform>();
         Transform parent = transform;
-        foreach (Transform mergeable in mergeables) {
-            if (parent == transform) {
+        foreach (Transform mergeable in mergeables)
+        {
+            if (parent == transform)
+            {
                 parent = mergeable;
                 continue;
             }
-        
+
             children.Clear();
             foreach (Transform child in mergeable)
                 children.Add(child);
