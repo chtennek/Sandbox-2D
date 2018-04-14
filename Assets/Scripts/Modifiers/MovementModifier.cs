34c0fee2f353c04819549368e51bb031e2d4e1c0
GoodKenneth <kenthesage@gmail.com>
2018-04-14 11:23:18 -0400

    Ad Time and Value Triggers

--- Assets/Scripts/Modifiers/MovementModifier.cs
+++ Assets/Scripts/Modifiers/MovementModifier.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovementModifier : FloatModifier
 {
-    private MoveControl target;
+    public MoveControl target;
 
     protected override float CurrentValue
     {
