0128d3f18d1a595bbe3183441af140c08247053a
GoodKenneth <kenthesage@gmail.com>
2018-04-18 18:06:19 -0400

    Add some gradients and shaders

---
+++ Assets/Assets/Scripts/Effects/Transition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Transition : MonoBehaviour {
+    public Material effect;
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+        if (effect == null)
+            return;
+        
+        Graphics.Blit(source, destination, effect);
+	}
+}
