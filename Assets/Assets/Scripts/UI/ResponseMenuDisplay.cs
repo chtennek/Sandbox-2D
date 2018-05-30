d7b82ef88399906119895b0173b41581d3ff089a
GoodKenneth <kenthesage@gmail.com>
2018-05-29 22:25:37 -0400

    Remove some meta files

--- Assets/Assets/Scripts/UI/ResponseMenuDisplay.cs
+++ Assets/Assets/Scripts/UI/ResponseMenuDisplay.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResponseMenuPopulator : MenuPopulator<ResponseDisplay, string> {
+public class ResponseMenuDisplay : MenuPopulator<ResponseDisplay, string> {
     public List<string> selections;
 
     public StringUnityEvent onSubmit;
